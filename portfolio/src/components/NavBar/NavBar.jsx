371708cabdbc4388461b9da9ca423dd5317f3524
TylerLo416 <lotyler416@gmail.com>
2024-03-29 13:56:30 -0700

    navbar improvements

--- portfolio/src/components/NavBar/NavBar.jsx
+++ portfolio/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import styles from './NavBar.module.css';
+import MarkImage from '../../Assets/Mark.png';
 
 const NavBar = () => {
   return (
-    <div className={styles.navbar}>
-      <Button variant="text" className={styles.button}>About</Button>
-      <Button variant="text" className={styles.button}>Portfolio</Button>
-      <Button variant="text" className={styles.button}>Contact</Button>
+    <div className={styles.container}>
+      <img src={MarkImage} alt='Mark' />
+      <div className={styles.navbar}>
+        <Button variant="text" className={styles.button}>About</Button>
+        <Button variant="text" className={styles.button}>Portfolio</Button>
+        <Button variant="text" className={styles.button}>Contact</Button>
+      </div>
     </div>
   )
 }
